7f13c14e9ed6ded14b09090f9de0a3efd2ad608e
stanleyctg03 <stanley.ching@softwire.com>
2026-09-14 12:23:49 +0100

    Allow users to enter longitude and latitude via terminal

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-console.log('hello world!');
+const readline = require('node:readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+async function main() {
+    const latitude = await askQuestion("Latitude: ");
+    const longitude = await askQuestion("Longitude: ");
+    rl.close();
+}
+
+function askQuestion(question: String) {
+    return new Promise((resolve) => {
+        rl.question(question, resolve);
+    });
+}
+
+main();
