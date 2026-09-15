0e3f4e664543085a093cb6c76a0b014c2fed7e6c
stanleyctg03 <stanley.ching@softwire.com>
2026-09-15 09:31:55 +0100

    Modified index.ts to be the entry point for public facing api, and added forecast.ts for specific API that requires wiring different apis

---
+++ src/forecast.ts
@@ -0,0 +1,45 @@
+import { getDataWithLatitudeAndLongitude, getDataForNextThreeHours } from "./weather-api.ts";
+import { getTemperature, getWeatherCode, isRaining } from "./weather-utils.ts";
+import { getNextThreeHours } from "./time-utils.ts";
+import { closeReadLine } from "./input.ts";
+import { callPostcodeApi, getLatitudeAndLongitude } from "./postcode-api.ts";
+import {weatherCodeMap} from "./constants.ts";
+
+
+export async function getMetaDataForNextThreeHours(postCode: String) {
+    try {
+        const result = {
+            temperature: [],
+            weatherType: [],
+            willRain: false,
+        };
+        const postCodeApiData = await callPostcodeApi(postCode);
+        const latitudeAndLongitude = await getLatitudeAndLongitude(postCodeApiData);
+
+        const fullTimeSeriesData = await getDataWithLatitudeAndLongitude(latitudeAndLongitude.latitude, latitudeAndLongitude.longitude);
+        const nextThreeHours = getNextThreeHours();
+        const filteredTimeSeriesData = getDataForNextThreeHours(fullTimeSeriesData, nextThreeHours);
+
+        for (let i = 0; i < filteredTimeSeriesData.length; i++) {
+            let timeSeriesEntry = filteredTimeSeriesData[i];
+
+            result.temperature.push(`${getTemperature(timeSeriesEntry).toFixed(2)}°C`);
+            result.weatherType.push(weatherCodeMap[getWeatherCode(timeSeriesEntry)]);
+
+            // console.log(`Temperature and Weather Type for Next ${i + 1} Hour: ${temperature.toFixed(2)}°C, ${description}`);
+
+            if (isRaining(timeSeriesEntry)) {
+                result.willRain = true;
+            }
+        }
+
+        return {
+            result
+        }
+
+    } catch (error) {
+        console.error(`An error occurred: ${error}`);
+    } finally {
+        closeReadLine();
+    }
+}
